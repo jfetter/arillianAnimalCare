8626fc44cf506829ea312ff858ad2b1539878826
Dev <jfetter@myms.io>
2018-06-30 14:37:47 -0700

    ...

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
 'use strict';
 
 $(document).ready(function(){
-    var selectedCard = '#dogs';
-    //$(selector).action()
-    $(selectedCard).removeClass('invisible');
-    $('#collapseOne').click(function(){
-        $('#dogs').removeClass('invisible');
-    })
-    $('#collapseTwo').click(function(){
-        console.error('#collapseTwo');
-        $('#goats').removeClass('invisible');
-    })
-    $('#collapseThree').click(function(){
-        $('#chickens').removeClass('invisible');
-    })
-    $('#collapseFour').click(function(){
-        $('#accomodations').removeClass('invisible');
-    })
+
     console.error("hello world");
 });
 
