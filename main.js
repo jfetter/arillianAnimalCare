1bb561c2a476c4623c67a24fb1917ce792e90ff2
Jillian Fetter <jillian.fetter@gmail.com>
2024-02-19 08:49:24 -0700

    brought in angularJS routing

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 'use strict';
 
-$(document).ready(function(){
-
-});
+//$(document).ready(function(){
+    console.log("main js loading "+ Date.now())
+    let app = angular.module("myApp", ["ngRoute"]);
+    app.config(function($routeProvider) {
+        console.log("app loaded " + Date.now())
+        $routeProvider
+            .when("/", {
+                templateUrl : "landingPages/main.html"
+            })
+            .when("/boowalla", {
+                templateUrl : "landingPages/boowalla.html"
+            })
+            .when("/arillians", {
+                templateUrl : "landingPages/arillians.html"
+            })
+            .when("/cuddlePuddle", {
+                templateUrl : "landingPages/cuddlePuddle.html"
+            });
+    });
+//});
 
